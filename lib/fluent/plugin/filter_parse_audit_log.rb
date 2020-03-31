6a932ae6711c58c08aaa4d2a66258a73f2602f5a
Chris Coetzee <chriscz@users.noreply.github.com>
2020-03-31 15:46:41 +0200

    Add unhex_min_length key

--- lib/fluent/plugin/filter_parse_audit_log.rb
+++ lib/fluent/plugin/filter_parse_audit_log.rb
@@ -8,11 +8,12 @@ class FluentParseAuditLogFilter < Fluent::Filter
   config_param :flatten, :bool, default: false
   config_param :unhex, :bool, default: false
   config_param :unhex_keys, :array, default: ['*'], value_type: :string 
+  config_param :unhex_min_length, :integer, default: 8
 
   def filter(tag, time, record)
     line = record[@key]
     return record unless line
-    AuditLogParser.parse_line(line, flatten: @flatten, unhex: @unhex, unhex_keys: @unhex_keys)
+    AuditLogParser.parse_line(line, flatten: @flatten, unhex: @unhex, unhex_keys: @unhex_keys, unhex_min_length: @unhex_min_length)
   rescue => e
     log.warn "failed to parse a audit log: #{line}", error_class: e.class, error: e.message
     log.warn_backtrace
